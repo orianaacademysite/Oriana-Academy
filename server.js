f86beb9bcdc6bdbc2b373b8d44fe4dff914464b4
orianaacademysite <orianaacademysite@gmail.com>
2026-07-16 14:05:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const path = require("path");
 
 const app = express();
 
+// Serve all static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
